df8f2ce80b91c26511be4fb9e5f897e15affe24d
Jin.xu <498270108@qq.com>
2020-06-22 14:48:54 +0800

    build

---
+++ .tea/entryFiles-development/index$.web.js
@@ -0,0 +1,6 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+
+require('../../components/tabs/tabs?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
+require('../../pages/index/index?hash=b4fc3e6438a66177e54078e072f2affabc98eebf');
+require('../../pages/cavans/cavans?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
